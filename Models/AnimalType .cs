5a4c7a15563986a16a7938648f9db200c872bbfc
luisaferRP <luisaramiresporras103@gmail.com>
2024-10-01 19:58:25 -0500

    feat:The class AnimalType was created

---
+++ Models/AnimalType .cs
@@ -0,0 +1,9 @@
+namespace API_FARM.Models
+{
+    public class AnimalType 
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
