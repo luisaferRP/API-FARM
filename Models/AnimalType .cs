47fb25dd443dba374f26ad2987f258811535b72a
luisaferRP <luisaramiresporras103@gmail.com>
2024-10-01 20:11:53 -0500

    feat:The AnimalType class was configured for later creation in the database

--- Models/AnimalType .cs
+++ Models/AnimalType .cs
@@ -1,9 +1,15 @@
 namespace API_FARM.Models
 {
+    [Table("animal_types")]
     public class AnimalType 
     {
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("name")]
         public required string Name { get; set; }
+        
+        [Column("description")]
         public string Description { get; set; }
     }
 }
